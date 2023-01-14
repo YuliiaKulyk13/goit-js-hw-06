14d003d531d06fab38dfa2ce3700077fc5dc3f16
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-01-14 16:10:54 -0500

    finished tasks

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formSubmission = document.querySelector(".login-form");
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const { elements } = event.currentTarget;
+  const emailValue = elements.email.value;
+  const passwordValue = elements.password.value;
 
+  if (emailValue === "" || passwordValue === "") {
+    return alert("All fields must be filled in!!!");
+  }
+  const formObject = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formObject);
+  event.currentTarget.reset();
+};
+
+formSubmission.addEventListener("submit", handleSubmit);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const textColor = document.querySelector(".color");
+const btnColor = document.querySelector(".change-color");
+
+const changeColor = (event) => {
+  const colorFunction = getRandomHexColor();
+  document.body.style.backgroundColor = colorFunction;
+  textColor.textContent = colorFunction;
+};
+
+btnColor.addEventListener("click", changeColor);
